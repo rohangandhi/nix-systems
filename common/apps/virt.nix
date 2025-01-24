c3895cd8b08544d74bc0317436b5bc82348abd7b
Rohan Gandhi <grohank@gmail.com>
2025-01-23 18:52:23 -0800

    adding virt-manager

--- common/apps/virt.nix
+++ common/apps/virt.nix
@@ -1,5 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, my-options, ... }: {
+
   environment.systemPackages = [
     pkgs.quickemu
   ];
+
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
+  users.users.${my-options.user.name}.extraGroups = [ "libvirtd" ];
 }
